fd9bde84b5ef7c3c6a77a39b79e748b9f9aee5a8
APM <albertopardomarti@gmail.com>
2022-06-06 20:50:22 +0200

    cambio en index.js para acceder a la carpetas 'controllers' y 'routes'

--- src/index.js
+++ src/index.js
@@ -3,25 +3,20 @@
 const express = require('express')
 const app = express()
 const port = process.env.NODE_PORT
-const os = require("os");
+const os = require('os')
 
 const drodriguezRouters = require('../src/routes/drodriguez')
 const chernandezRouters = require('../src/routes/chernandez')
+const albertoRouters = require('../src/routes/alberto')
 
 app.use(chernandezRouters)
 app.use(drodriguezRouters)
+app.use(albertoRouters)
 
 app.get('/', (req, res) => {
   res.send({
     msg: 'Hola Mundo.',
     hostname: os.hostname()
-  })
-})
-
-
-app.get('/alberto', (req, res) => {
-  res.send({
-      msg: "Rama de Alberto"
   })
 })
 
