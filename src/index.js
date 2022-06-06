ba5bedc8433e332621b66be6b21b2694c9a147db
carolhm92 <carolhm92@hotmail.com>
2022-06-05 22:16:08 -0600

    conflicto

--- src/index.js
+++ src/index.js
@@ -5,28 +5,19 @@ const app = express()
 const port = process.env.NODE_PORT
 const chernandezRouters = require('../src/routes/chernandez')
 const os = require("os");
+
+const drodriguezRouters = require('../src/routes/drodriguez')
+const chernandezRouters = require('../src/routes/chernandez')
+
 app.use(chernandezRouters)
-
-
-
+app.use(drodriguezRouters)
 
 app.get('/', (req, res) => {
   res.send({
-      msg: "Hola Mundo.",
-      hostname: os.hostname()
+    msg: 'Hola Mundo.',
+    hostname: os.hostname()
   })
 })
-
-
-
-
-
-
-
-
-
-
-
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
