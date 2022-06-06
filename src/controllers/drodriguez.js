94903a1aed5ad6456af1651b4c44b59d1c2832c4
Carlos Rodriguez <cdaniel2r.dr@gmail.com>
2022-06-05 21:18:09 -0400

    Agregar archivo de ruta drodriguez
    /drodriguez

---
+++ src/controllers/drodriguez.js
@@ -0,0 +1,10 @@
+const os = require('os')
+exports.getData = (req, res) => {
+  res.send({
+    name: 'Carlos Daniel Rodriguez',
+    profession: 'Ing en informatio',
+    country: 'Santiago / Chile',
+    msg: 'Hello world',
+    hostname: os.hostname()
+  })
+}
